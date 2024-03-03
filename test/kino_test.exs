7c60ac30d37d3827ea644ed0bde5b5b624bea007
Philip Capel <pcapel@users.noreply.github.com>
2024-03-02 21:23:18 -0600

    chore: fix test structure

--- test/kino_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule KinoBio.KinoTest do
-  use ExUnit.Case
-  doctest KinoBio.Kino
-
-  test "greets the world" do
-    flunk
-  end
-end
---
+++ test/sequence/dna_test.exs
@@ -0,0 +1,7 @@
+defmodule KinoBio.Sequence.DnaTest do
+  use ExUnit.Case, async: true
+
+  alias KinoBio.Sequence
+
+  doctest Sequence
+end
