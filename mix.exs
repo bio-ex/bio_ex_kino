aea2d72cecc9d75a24a458a1aa59e28731d5405a
Philip Capel <pcapel@users.noreply.github.com>
2024-03-16 17:09:55 -0500

    feature: add seqviz as rendering for sequence type dna
    This is a really good library for this type of visualization! Whee!
    
    There are still some places where we can improve how things are working,
    but this is a good first pass.

--- mix.exs
+++ mix.exs
@@ -9,6 +9,7 @@ defmodule Bio.Kino.MixProject do
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
+    |> with_coverage()
   end
 
   def application do
@@ -20,7 +21,22 @@ defmodule Bio.Kino.MixProject do
   defp deps do
     [
       {:kino, "~> 0.12.0"},
-      {:bio_ex_sequence, "~> 0.1.1"}
+      {:bio_ex_sequence, "~> 0.1.1"},
+      {:bio_ex_restriction, "~> 0.1.1"},
+      {:excoveralls, "~> 0.18", only: :test}
     ]
+  end
+
+  defp with_coverage(list) do
+    Keyword.merge(list,
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test,
+        "coveralls.cobertura": :test
+      ]
+    )
   end
 end
